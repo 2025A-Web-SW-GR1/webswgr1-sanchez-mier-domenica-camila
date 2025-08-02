79268fda65afdfb980c8994baf382268fc46704f
Camila SÃanchez <domenica.sanchez@epn.edu.ec>
2025-08-02 08:17:08 -0500

    Feat: Class 29-07-2025

--- 02-servidor/ejemplo-servidor/src/casa/casa.controller.ts
+++ 02-servidor/ejemplo-servidor/src/casa/casa.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query, Res, Session } from '@nestjs/common';
 import { CasaService } from './casa.service';
 import { FindManyOptions } from 'typeorm/find-options/FindManyOptions';
 import { Like } from 'typeorm';
 import { BuscarDto } from './dto/buscar.dto';
 import { Casa } from './casa.entity';
 import { CrearEditarBaseDTO } from './dto/crear-editar.base.dto';
+import { CasaEditarDto } from './dto/casa-editar.dto';
+
 @Controller('api/casa')
 export class CasaController {
     constructor(
@@ -35,5 +37,31 @@ export class CasaController {
             parametrosCuerpo.imageUrl
     }
 
+    @Put(':id')
+    actualizarUnoPorId(
+        @Body() parametrosCuerpo: CasaEditarDto,
+        @Param('id') id: string
+    ) {
+        try {
+            return this.casaService.actualizarUnoPorId(
+                parametrosCuerpo,
+                +id,
+            );
+        } catch (e) {
+            throw new NotFoundException('Registro no encontrado, o error del servidor')
+        }
+    }
+    @Delete(':id')
+    async eliminarUnoPorId(
+        @Param('id') id: string,
+    ) {
+        const response = await this.casaService.eliminarUnoPorId(+id);
+        if (response.affected === 1) {
+            return {
+                mensaje: 'Eliminado correctamente',
+            };
+        }
+        throw new BadRequestException('No se encontro el registro');
+    }
 
 }
--- 02-servidor/ejemplo-servidor/src/casa/casa.entity.ts
+++ 02-servidor/ejemplo-servidor/src/casa/casa.entity.ts
@@ -3,7 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Casa {
   @PrimaryGeneratedColumn()
-  id: number;
+  id?: number;
 
   @Column({ length: 500 })
   nombre: string;
@@ -11,6 +11,23 @@ export class Casa {
   @Column()
   valor: number;
 
-  @Column()
-  imagenURL: string;
+  @Column({ nullable: true })
+  imagenURL?: string;
+
+  @Column({ nullable: true })
+  fileContentType?: string;
+
+  @Column({ nullable: true })
+  filename?: string;
+
+  @Column({ nullable: true })
+  fileID?: string;
+
+  // Usado para los datos de sesion
+
+  @Column({ nullable: true })
+  username: string;
+
+  @Column({ nullable: true })
+  password: string;
 }
